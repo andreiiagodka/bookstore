db62d68af3163370bc231fadd5f61b9f1112778c
andrei_iagodka <andrei.iagodka@gmail.com>
2019-03-25 14:04:18 +0200

    Fix devise rendering errors and flash messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
+  get '/users', to: redirect('/users/sign_up')
   root 'pages#home'
 
   resources :books, only: [:index, :show]
