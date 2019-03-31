cbf0a1d9d87b19252257f63850babe23c11e0e91
andreiiagodka <andrei.iagodka@gmail.com>
2019-04-01 02:26:34 +0300

    Refactor books and add tab for authors

--- app/admin/books.rb
+++ app/admin/books.rb
@@ -9,8 +9,39 @@ ActiveAdmin.register Book do
   config.filters = false
 
   index do
-    render 'admin/books/index', context: self
+    selectable_column
+    column :cover do |book|
+      book.cover_image(:cart)
+    end
+    column :categories do |book|
+      book.categories_as_string
+    end
+    column :name
+    column :authors do |book|
+      book.authors_as_string
+    end
+    column :description do |book|
+      book.short_description
+    end
+    column :price do |book|
+      t('price', price: book.price)
+    end
+    actions
   end
 
-  form partial: 'form'
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :description, as: :simplemde_editor
+      f.input :price
+      f.input :publication_year
+      f.input :height
+      f.input :width
+      f.input :depth
+      f.input :material
+      f.input :categories, as: :check_boxes, collection: Category.all.map { |category| [category.name, category.id] }
+      f.input :authors, as: :check_boxes, collection: Author.all.map { |author| [author.name, author.id] }
+      f.actions
+    end
+  end
 end
