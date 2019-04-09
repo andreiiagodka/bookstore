949e78df447d3f5581fe6c8a777336c1c9f403de
andreiiagodka <andrei.iagodka@gmail.com>
2019-04-10 02:18:44 +0300

    Rethink spec's logic for address controller and add for book controler

--- spec/controllers/addresses_controller_spec.rb
+++ spec/controllers/addresses_controller_spec.rb
@@ -2,36 +2,31 @@ require 'rails_helper'
 
 RSpec.describe AddressesController, type: :controller do
   let(:user) { create(:user) }
-  let(:params) { attributes_for(:address, cast: 'billing') }
 
   before { allow(controller).to receive(:current_user).and_return(user) }
 
-  describe 'POST #create' do
-    context 'valid attributes' do
-      before do
-        allow_any_instance_of(AddressForm).to receive(:save).with(user).and_return(true)
-        post :create, xhr: true, params: { id: 1, address_form: params }
-      end
-
-      it do
-        is_expected.to redirect_to root_path
-        is_expected.to set_flash[:success].to I18n.t('message.success.address.update', type: params[:cast].capitalize)
-      end
+  describe 'POST create' do
+    let(:address_params) do
+      { address_form: attributes_for(:address, cast: 'billing') }
     end
 
-    context 'invalid attributes' do
-      let(:error) { I18n.t('message.error.address.text_field') }
+    before { post :create, params: address_params }
 
-      before do
-        allow_any_instance_of(AddressForm).to receive(:save).with(user).and_return(false)
-        allow_any_instance_of(AddressForm).to receive_message_chain(:errors, :full_messages, :to_sentence).and_return(error)
-        post :create, xhr: true, params: { address_form: params }
-      end
+    it 'return redirect response' do
+      expect(response).to have_http_status(302)
+    end
+  end
 
-      it do
-        is_expected.to redirect_to root_path
-        is_expected.to set_flash[:danger].to error
-      end
+  describe 'PUT update' do
+    let(:address) { create(:address, :billing, addressable: user) }
+    let(:address_params) do
+      { address_form: attributes_for(:address), id: address.id }
+    end
+
+    before { put :update, params: address_params }
+
+    it 'return redirect response' do
+      expect(response).to have_http_status(302)
     end
   end
 end
