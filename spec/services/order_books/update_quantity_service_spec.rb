2c5778037256c97ea05639ed76d40204a57d415e
andrei_iagodka <andrei.iagodka@gmail.com>
2019-04-15 17:55:03 +0300

    Add specs for services

---
+++ spec/services/order_books/update_quantity_service_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+require 'pry'
+RSpec.describe OrderBooks::UpdateQuantityService do
+  subject(:update_quantity_service) { described_class.new(order_book, quantity_action) }
+
+  let(:quantity_actions) { OrderBooks::UpdateQuantityService::QUANTITY_ACTION }
+
+  context 'increment order book quantity' do
+    let(:order_book) { create(:order_book) }
+    let(:quantity_action) { quantity_actions[:increment] }
+
+    it { expect { update_quantity_service.call }.to change(order_book, :quantity).by(1) }
+
+    it do
+      expect(order_book).to receive(:increment!).with(:quantity)
+      update_quantity_service.call
+    end
+  end
+
+  context 'decrement order book quantity' do
+    let(:order_book) { create(:order_book, quantity: 2) }
+    let(:quantity_action) { quantity_actions[:decrement] }
+
+    it { expect { update_quantity_service.call }.to change(order_book, :quantity).by(-1) }
+
+    it do
+      expect(order_book).to receive(:decrement!).with(:quantity)
+      update_quantity_service.call
+    end
+  end
+
+  context 'decrement when quantity equals 1' do
+    let(:order_book) { create(:order_book, quantity: 1) }
+    let(:quantity_action) { quantity_actions[:decrement] }
+
+    it { expect { update_quantity_service.call }.to change(order_book, :quantity).by(0) }
+  end
+end
