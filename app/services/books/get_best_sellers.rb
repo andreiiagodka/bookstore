29a94eaafe324a949b9d8fe944b53cd88267fd9d
andrei_iagodka <andrei.iagodka@gmail.com>
2019-05-13 17:57:03 +0300

    Rewrite getting best sellers into service

---
+++ app/services/books/get_best_sellers.rb
@@ -0,0 +1,9 @@
+class Books::GetBestSellers
+  BEST_SELLERS_QUANTITY = 4
+  
+  def call
+    Book.find(
+      Order.completed.joins(:order_books).group(:book_id).order('sum("order_books"."quantity") DESC').count.keys
+    ).first(BEST_SELLERS_QUANTITY)
+  end
+end
