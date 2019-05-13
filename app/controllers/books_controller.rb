279a3b8c3a3f818d3174d0c9f464676c39f75db5
andrei_iagodka <andrei.iagodka@gmail.com>
2019-05-13 17:54:33 +0300

    Rethink logic of filtering books and orders

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,15 +9,7 @@ class BooksController < ApplicationController
   decorates_assigned :selected_books, :book
 
   def index
-    @scoped_books = Books::GetCategoryBooksService.new(@books, params[:category_id]).call
-    @pagy, @selected_books = pagy(@scoped_books.public_send(get_filter), items: BOOKS_PER_PAGE)
-  end
-
-  def show; end
-
-  private
-
-  def get_filter
-    @filter = BOOK_FILTERING_ORDER.include?(params[:filter]&.to_sym) ? params[:filter] : DEFAULT_BOOK_FILTERING_ORDER
+    @scoped_books = Books::GetCategoryBooksService.new(@books, params).call
+    @pagy, @selected_books = pagy(@scoped_books, items: BOOKS_PER_PAGE)
   end
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,20 +4,13 @@ class OrdersController < ApplicationController
 
   ORDERS_PER_PAGE = 10
 
-  include Filtering
   include Pagy::Backend
 
   decorates_assigned :order
 
   def index
-    @pagy, @orders = pagy(current_user.orders.public_send(get_filter), items: ORDERS_PER_PAGE)
+    @pagy, @orders = pagy(Orders::GetUserOrdersService.new(current_user, params).call, items: ORDERS_PER_PAGE)
   end
 
   def show; end
-
-  private
-
-  def get_filter
-    @filter = ORDER_FILTERING_ORDER.include?(params[:filter]&.to_sym) ? params[:filter] : DEFAULT_ORDER_FILTERING_ORDER
-  end
 end
